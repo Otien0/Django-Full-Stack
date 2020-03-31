84e7637824c28c679643098d72ed3671c279c77c
mourice-oduor <otienomaurice364@gmail.com>
2020-03-31 16:13:15 -0400

    Django Level 1&2

---
+++ Back-End/Django Level 1&2/first_project/first_app/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+class Topic(models.Model):
+    top_name = models.CharField(max_length=264,unique=True)
+
+    def __str__(self):
+        return self.top_name
+
+class Webpage(models.Model):
+    topic = models.ForeignKey(Topic,on_delete=models.CASCADE)
+    name = models.CharField(max_length=264,unique=True)
+    url = models.URLField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class AccessRecord(models.Model):
+    name = models.ForeignKey(Webpage,on_delete=models.CASCADE)
+    date = models.DateField()
+
+    def __str__(self):
+        return str(self.date)
---
+++ Back-End/Django Level 1&2/first_project/first_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from first_app.models import Topic,Webpage,AccessRecord
+# Create your views here.
+
+def index(request):
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {"access_records": webpages_list}
+    return render(request,'first_app/index.html',context=date_dict)
