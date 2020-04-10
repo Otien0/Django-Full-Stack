497288ed79e01f8b6aa3b281e97370291dc25fc9
mourice-oduor <otienomaurice364@gmail.com>
2020-04-10 11:04:56 -0400

    update ProTwo

--- Back-End/Django_Level_Three/ProTwo/appTwo/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from appTwo.models import User
-
-class NewUserForm(forms.ModelForm):
-    class Meta():
-        model = User
-        fields = '__all__'
--- Back-End/Django_Level_Three/ProTwo/appTwo/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class User(models.Model):
-    first_name = models.CharField(max_length=128)
-    last_name = models.CharField(max_length=128)
-    email = models.EmailField(max_length=254,unique=True)
