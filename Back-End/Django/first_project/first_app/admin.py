c2aa938646e51d2811c5470018fac65a8cf4e3e9
mourice-oduor <otienomaurice364@gmail.com>
2020-04-24 00:23:17 -0400

    Update files

--- Back-End/Django/first_project/first_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from first_app.models import AccessRecord,Topic,Webpage
-
-# Register your models here.
-admin.site.register(AccessRecord)
-admin.site.register(Topic)
-admin.site.register(Webpage)
--- Back-End/Django/first_project/first_app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from first_app import views
-
-urlpatterns = [
-    url(r'^$',views.index,name='index'),
-]
