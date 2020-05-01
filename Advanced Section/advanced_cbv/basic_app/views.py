3c7cef0c3f8e5e3cf152dd26d8fd1355025ce104
mourice-oduor <otienomaurice364@gmail.com>
2020-05-01 10:30:03 -0400

    Class Based Views

---
+++ Advanced Section/advanced_cbv/basic_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views.generic import View
+from django.http import HttpResponse
+
+# Original Function View:
+#
+# def index(request):
+#     return render(request,'index.html')
+
+class CBView(View):
+    def get(self,request):
+        return HttpResponse('Class Based Views are Cool!')
