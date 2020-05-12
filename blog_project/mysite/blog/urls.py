1db967784d5665a6ca078a3d555872133e6e4176
mourice-oduor <otienomaurice364@gmail.com>
2020-05-11 23:45:23 -0400

    Update blog/urls.py file

--- blog_project/mysite/blog/urls.py
+++ blog_project/mysite/blog/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
+from django.urls import path
 from . import views
+
 
 
 urlpatterns = [
     url(r'^$',views.PostListView.as_view(),name='post_list'),
     url(r'^about/$',views.AboutView.as_view(),name='about'),
+    path('', views.AboutView.as_view(), name='about'),
     url(r'^post/(?P<pk>\d+)$', views.PostDetailView.as_view(), name='post_detail'),
     url(r'^post/new/$', views.CreatePostView.as_view(), name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.PostUpdateView.as_view(), name='post_edit'),
