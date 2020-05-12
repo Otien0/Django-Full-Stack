7b2a913791a4aa5a7251e68d443ede49d11fee35
mourice-oduor <otienomaurice364@gmail.com>
2020-05-11 22:58:06 -0400

    Update urls.py file

--- blog_project/mysite/mysite/urls.py
+++ blog_project/mysite/mysite/urls.py
@@ -16,12 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
-
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'', include('blog.urls')),
     url(r'^accounts/login/$', auth_views.LoginView.as_view(template_name='login.html'),  name='login'),
     url(r'^accounts/logout/$', auth_views.LogoutView.as_view(template_name='logged_out.html'), name='logout', kwargs={'next_page': '/'}),
+    path('', include('blog.urls')),
 ]
+if settings.DEBUG: # new
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
