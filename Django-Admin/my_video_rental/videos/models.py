c15e6fc93663c7d02de4934610fdf3b97f2d9fb8
mourice-oduor <otienomaurice364@gmail.com>
2020-05-18 02:20:48 -0400

    Update models

--- Django-Admin/my_video_rental/videos/models.py
+++ Django-Admin/my_video_rental/videos/models.py
@@ -5,7 +5,15 @@ class Movie(models.Model):
     length = models.PositiveIntegerField()
     release_year = models.PositiveIntegerField()
 
+    def __str__(self):
+        return self.title
+
 class Customer(models.Model):
     first_name = models.CharField(max_length=256)
     last_name = models.CharField(max_length=256)
     phone = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+    
