9b94dd123d2c85a10c999ab8e911fcbbc7cfec2d
mourice-oduor <otienomaurice364@gmail.com>
2020-05-17 22:09:53 -0400

    Update urls.py file

--- Back-End/Django Level 1&2/proTwo/proTwo/urls.py
+++ Back-End/Django Level 1&2/proTwo/proTwo/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from django.conf.urls import include
+from django.conf.urls import include, path
+from django.conf import settings
 from appTwo import views
 
 urlpatterns = [
@@ -23,3 +24,13 @@ urlpatterns = [
     url(r'^users/',include('appTwo.urls')),
     url(r'^admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+
+        # For django versions before 2.0:
+        # url(r'^__debug__/', include(debug_toolbar.urls)),
+
+    ] + urlpatterns
