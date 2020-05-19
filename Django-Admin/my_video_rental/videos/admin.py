f965e606ba57e406e331259aa5b84b6ad090de96
mourice-oduor <otienomaurice364@gmail.com>
2020-05-18 20:55:29 -0400

    Adding search_fields

--- Django-Admin/my_video_rental/videos/admin.py
+++ Django-Admin/my_video_rental/videos/admin.py
@@ -5,5 +5,7 @@ from . import models
 class MovieAdmin(admin.ModelAdmin):
     fields = ['release_year','title','length']
 
+    search_fields = ['title','length']
+
 admin.site.register(models.Customer)
 admin.site.register(models.Movie,MovieAdmin)
