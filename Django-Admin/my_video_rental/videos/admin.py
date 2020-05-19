bca6635a26536b6bbfc2f56e44b91bdca6273900
mourice-oduor <otienomaurice364@gmail.com>
2020-05-18 22:18:30 -0400

    Adding filters

--- Django-Admin/my_video_rental/videos/admin.py
+++ Django-Admin/my_video_rental/videos/admin.py
@@ -7,5 +7,7 @@ class MovieAdmin(admin.ModelAdmin):
 
     search_fields = ['title','length']
 
+    list_filter = ['release_year','title','length']
+
 admin.site.register(models.Customer)
 admin.site.register(models.Movie,MovieAdmin)
