d214aef80c41b872d71ada3f98a1f9a20256c8e4
mourice-oduor <otienomaurice364@gmail.com>
2020-05-19 19:28:42 -0400

    Update admin.py file

--- Django-Admin/my_video_rental/videos/admin.py
+++ Django-Admin/my_video_rental/videos/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+from django.urls import path
 from . import models
 
 # Register your models here.
@@ -14,6 +16,19 @@ class MovieAdmin(admin.ModelAdmin):
     list_editable = ['length']
 
     change_list_template = 'admin/videos/change_list.html'
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path('fontsize/<int:size>/', self.change_font_size)
+        ]
+        return custom_urls + urls
+
+    def change_font_size(self, request, size):
+        self.model.objects.all().update(font_size=size)
+        self.message_user(request, 'font size set successfully')
+        return HttpResponseRedirect("../")
+
 
 admin.site.register(models.Customer)
 admin.site.register(models.Movie,MovieAdmin)
