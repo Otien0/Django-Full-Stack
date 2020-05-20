9869c0d6f53b34d1ba20bea60c25595381f0007d
mourice-oduor <otienomaurice364@gmail.com>
2020-05-19 21:11:29 -0400

    Update settings.py file

--- Django-Admin/my_video_rental/my_video_rental/settings.py
+++ Django-Admin/my_video_rental/my_video_rental/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'my_video_rental.apps.SuitConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
