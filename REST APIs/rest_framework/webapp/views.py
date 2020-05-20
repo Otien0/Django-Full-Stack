c2743a71838f728e3ff60be7d6b8df3e3e343b6a
mourice-oduor <otienomaurice364@gmail.com>
2020-05-20 13:48:58 -0400

    Update views.py

--- REST APIs/rest_framework/webapp/views.py
+++ REST APIs/rest_framework/webapp/views.py
@@ -1,3 +1,23 @@
+###### Here we are requesting an API,  ####
+###### and getting back the JSON       #####
+############################################
+
 from django.shortcuts import render
+from django.http import HttpResponse
+from  django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from . models import employees
+from . serializers import employeesSerializer
 
 # Create your views here.
+class employeeList(APIView):
+
+    def get(self, request):
+        employees1 = employees.objects.all()
+        serializer = employeesSerializer(employees1, many=True)
+        return Response(serializer.data)
+
+    def post(self):
+        pass
