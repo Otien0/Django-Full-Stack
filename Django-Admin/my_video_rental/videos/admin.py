8e25865c124ac9bc2384543898abc056193f2fa2
mourice-oduor <otienomaurice364@gmail.com>
2020-05-19 20:03:47 -0400

    Update admin.py file

--- Django-Admin/my_video_rental/videos/admin.py
+++ Django-Admin/my_video_rental/videos/admin.py
@@ -2,16 +2,17 @@ from django.contrib import admin
 from django.http import HttpResponseRedirect
 from django.urls import path
 from . import models
+from django.utils.html import format_html
 
 # Register your models here.
 class MovieAdmin(admin.ModelAdmin):
-    fields = ['release_year','title','length']
+    fields = ['release_year','title','length','font_size']
 
     search_fields = ['title','length']
 
-    list_filter = ['release_year','title','length']
+    list_filter = ['release_year','title','length','font_size']
 
-    list_display = ['title','release_year','length','font_size']
+    list_display = ['title','release_year','length','font_size_html_display']
 
     list_editable = ['length']
 
@@ -28,6 +29,11 @@ class MovieAdmin(admin.ModelAdmin):
         self.model.objects.all().update(font_size=size)
         self.message_user(request, 'font size set successfully')
         return HttpResponseRedirect("../")
+
+    def font_size_html_display(self, obj):
+        return format_html(
+            f'<span style="font-size: {obj.font_size}px;">{obj.font_size}</span>'
+        )
 
 
 admin.site.register(models.Customer)
