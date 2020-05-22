4eedb6bb96451a7f8e959bce7c195914604e5802
mourice-oduor <otienomaurice364@gmail.com>
2020-05-21 20:00:08 -0400

    Update urls.py file

--- REST APIs/rest_framework/rest_framework/urls.py
+++ REST APIs/rest_framework/rest_framework/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
+from webapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^employess/', views.employeeList.as_view()),
+
 ]
