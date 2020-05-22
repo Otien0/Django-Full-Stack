3f7cb789085d33dfb8139b7c5d9c43711f8295dc
mourice-oduor <otienomaurice364@gmail.com>
2020-05-22 14:07:24 -0400

    Update settings

--- REST APIs/rest_framework/rest_framework/settings.py
+++ REST APIs/rest_framework/rest_framework/settings.py
@@ -41,6 +41,14 @@ INSTALLED_APPS = [
     'webapp',
 ]
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
